3979a36383b46fae5d6aeae424c0734b95e0abe7
Paul Ulerick <p.ulerick17@gmail.com>
2022-11-18 13:51:25 -0500

    Wiped code, restarting as new idea

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react'
-import TodoList from './TodoList';
 import { nanoid } from 'nanoid'
-import { Session } from 'express-session';
+
 
 // useRef hook allows you to reference input data from the page in a variable
 // useState allows you to store the data from a page in state
 // useEffect hook 
 
-const LSKEY = 'todoApp.todos'
-
 function App() {
-  if (Session.authenticated) {
+  if (authenticated) {
     return (
       UserPage()
     )
@@ -79,55 +76,12 @@ function LandingPage() {
 }
 
 function UserPage() {
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
+  const [exercises, setExercises] = useState([])
+  const exerciseNameRef = useRef()
   
-  // Called once when component loads
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LSKEY))
-    if (storedTodos) {
-      setTodos(storedTodos)
-    }
-  }, [])
-
-  // Called whenever something in the array at the end of the function changes
-  useEffect(() => {
-    // Inserts the todos into local storage
-    localStorage.setItem(LSKEY, JSON.stringify(todos))
-  }, [todos])
-
-  function toggleTodo(id) {
-    // Creates copy of todos list(in react you shouldn't modify state components directly)
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
-  }
-
-  function handleAddTodo(e) {
-    // Calls the element currently being referenced
-    const name = todoNameRef.current.value
-
-    if (name === '') return
-    setTodos(prevTodos => {
-      return [...prevTodos, { id: nanoid(), name: name, complete: false}]
-    })
-    // Resets the todo value after being called to clear the input
-    todoNameRef.current.value = null
-  }
-
-  function handleClearTodos() {
-    const newTodos = todos.filter(todo => !todo.complete)
-    setTodos(newTodos)
-  }
-
   return (
     <>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <button onClick={handleClearTodos}>Clear Completed</button>
-      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+
     </>
   );
 }
