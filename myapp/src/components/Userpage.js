c7e27583ad899097e6749d46ba3181129caddbee
Paul Ulerick <p.ulerick17@gmail.com>
2022-12-02 16:17:06 -0500

    Can create and display workouts on frontend now just need to make it look better

--- myapp/src/components/Userpage.js
+++ myapp/src/components/Userpage.js
@@ -1,24 +1,62 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
-import WorkoutInput from './WorkoutInput';
+import { nanoid } from 'nanoid';
+import NumericInput from 'react-numeric-input';
+
+import Workout from './Workout';
 import WorkoutList from './WorkoutList';
 
+const LS_KEY =  'workoutApp.exercise';
+
 export default function Userpage() {
+  const exerciseNameRef = useRef();
+  const exerciseRepsRef = useRef();
+  const exerciseWeightRef = useRef();
+
   const [exercises, setExercises] = useState([]);
 
-
   function handleAddExercise(e) {
-    
+    const name = exerciseNameRef.current.value;
+    const reps = exerciseRepsRef.current.value;
+    const weight = exerciseWeightRef.current.value;
+
+    if (name === '') return
+    setExercises(prevExercises => {
+      return [...prevExercises, {id: nanoid(), name: name, reps: reps, weight: weight}]
+    })
+
+    exerciseNameRef.current.value = null;
+    exerciseRepsRef.current.value = null;
+    exerciseWeightRef.current.value = null;
   };
+
+  useEffect(() => {
+    const storedExercises = JSON.parse(localStorage.getItem(LS_KEY));
+    if (storedExercises) {
+      setExercises(storedExercises)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(exercises))
+  }, [exercises])
 
   return (
     <>
       <div className='user-container'>
-        <h1>Workout Center</h1>
         <div>
-          <h2>Monday</h2>
-          <WorkoutList exercise={exercises}/>
-          <button>Add an exercise</button>
+            <WorkoutList exercises={exercises}/>
+            <label>Enter exercise name</label>
+            <input ref={exerciseNameRef} type='text' required/>
+
+            <label>Enter amount of reps</label>
+            <input ref={exerciseRepsRef} required/>
+
+            <label>Enter exercise weight</label>
+            <input ref={exerciseWeightRef} required/>
+
+            <button type='submit' onClick={handleAddExercise}>Create Exercise</button>
+
         </div>
       </div>
     </>
--- myapp/src/components/Workout.js
+++ myapp/src/components/Workout.js
@@ -3,11 +3,12 @@ import React from 'react'
 export default function Workout({ workout }) {
   return (
     <div>
-      <label>
-      {workout.name}
-      </label>
-      {workout.weight}
-      {workout.reps}
+      <label>Exercise name</label>
+      <p>{workout.name}</p>
+      <label>Exercise Weight</label>
+      <p>{workout.weight}</p>
+      <label>Exercise Reps</label>
+      <p>{workout.reps}</p>
     </div>
   )
 }
