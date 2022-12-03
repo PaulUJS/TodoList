05344825918060bf22d86d817c48f71c562b3dd4
Paul Ulerick <p.ulerick17@gmail.com>
2022-12-03 16:26:20 -0500

    fixed the api not linking with frontend

--- myapp/src/components/Userpage.js
+++ myapp/src/components/Userpage.js
@@ -1,7 +1,6 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { nanoid } from 'nanoid';
-
+import React, { useState, useEffect } from 'react'
 import WorkoutList from './WorkoutList';
+import WorkoutForm from './WorkoutForm';
 
 const LS_KEY =  'workoutApp.exercise';
 
@@ -31,48 +30,16 @@ export default function Userpage() {
 
 function Day({ day }) {
 
-  // useRef allows me to keep values through renders
-  const exerciseNameRef = useRef();
-  const exerciseRepsRef = useRef();
-  const exerciseWeightRef = useRef();
-
   // useState allows use to track the state of the app
   // The state is just data that needs to be tracked like inputs
   // set is setting the state to reflect the state when it's called, basically updating it
   const [isShown, setIsShown] = useState(false);
   const [exercises, setExercises] = useState([]);
-
+  
   function showAddExercise(e) {
     setIsShown(current => !current);
   };
-
-  async function handleAddExercise(e) {
-    const name = exerciseNameRef.current.value;
-    const reps = exerciseRepsRef.current.value;
-    const weight = exerciseWeightRef.current.value;
-
-    // Sets the state of the exercises relative to the last input
-    if (name === '') return
-    setExercises(prevExercises => {
-      return [...prevExercises, {id: nanoid(), day: JSON.stringify(day), name: name, reps: reps, weight: weight}]
-    })
-
-    // Sends a post request to the backend api to add the workout to the db
-    const response = await fetch(`/api/workouts/${JSON.stringify(day)}`,{
-      method: 'POST',
-      body: JSON.stringify(exercises),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-    const json = await response.json();
-
-    exerciseNameRef.current.value = null;
-    exerciseRepsRef.current.value = null;
-    exerciseWeightRef.current.value = null;
-  };
-
+  
   // useEffect allows me to peform side effects in components
   // This includes fetching data, updating dom, etc
   // useEffect accepts 2 arguments, the second is optional
@@ -80,7 +47,7 @@ function Day({ day }) {
 
     // Grabs the exercises from the db and displays them on page load
     async function fetchExercises() {
-      const response = await fetch(`/api/workouts/${JSON.stringify(day)}`);
+      const response = await fetch(`http://localhost:4000/api/workouts/`);
       const json = await response.json();
       if (response.ok) {
         setExercises(json)
@@ -99,18 +66,7 @@ function Day({ day }) {
       <WorkoutList exercises={exercises}/>
         <button  className='input-toggle' onClick={showAddExercise}>Add Exercise</button>
         {isShown && (
-        <form className='exercise-input'>
-          <label>Enter exercise name</label>
-          <input ref={exerciseNameRef} type='text' required/>
-
-          <label>Enter amount of reps</label>
-          <input ref={exerciseRepsRef} type='number' required/>
-
-          <label>Enter exercise weight</label>
-          <input ref={exerciseWeightRef} type='number' required/>
-
-          <button type='submit' onClick={handleAddExercise}>Submit Exercise</button>
-        </form>
+          <WorkoutForm/>
         )}
     </>
   )
---
+++ myapp/src/components/WorkoutForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+export default function WorkoutForm() {
+  const [name, setName] = useState('');
+  const [weight, setWeight] = useState('');
+  const [reps, setReps] = useState('');
+  const day = 'tuesday'
+  const [error, setError] = useState(null);
+
+  async function sendWorkout(e) {
+    e.preventDefault();
+
+    const workout = {day, name, weight, reps}
+
+    // Sends a post request to the backend api to add the workout to the db
+    const response =  fetch('http://localhost:4000/api/workouts/',{
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const json =  await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
+      console.log('error')
+    }
+    if (response.ok) {
+      setName('');
+      setWeight('');
+      setReps('');
+      setError(null);
+      console.log('new workout added', json);
+    }
+  }
+
+  return (
+    <>
+      <form className='exercise-input'>
+        <label>Enter exercise name</label>
+        <input onChange={(e) => setName(e.target.value)} value={name} type='text' required/>
+
+        <label>Enter amount of reps</label>
+        <input onChange={(e) => setReps(e.target.value)} value={reps} type='number' required/>
+
+        <label>Enter exercise weight</label>
+        <input onChange={(e) => setWeight(e.target.value)} value={weight} type='number' required/>
+
+        <button type='submit' onClick={sendWorkout}>Submit Exercise</button>
+        {error && <div className='error'>{error}</div>}
+      </form>
+    </>
+  )
+}
+
